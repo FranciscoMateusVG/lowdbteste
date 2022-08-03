e7f51cb49dbb0eb64f46e45a58275058c78b3601
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-08-02 21:30:12 -0300

    Update mongoDB.ts

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
 const uri =
-  'mongodb+srv://admin:admin@zawarudo.saxte.gcp.mongodb.net/?retryWrites=true&w=majority'
+  ''
 export const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1
 })
